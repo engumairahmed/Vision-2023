ad88f89fdac92118ad83cf56f6f6e78bd06348f0
engumairahmed <eng.umair.a@gmail.com>
2023-08-24 01:37:15 +0500

    Update layout.blade.php

--- resources/views/patient/layout.blade.php
+++ resources/views/patient/layout.blade.php
@@ -418,6 +418,13 @@
     <script src="{{asset("js/sb-admin-2.min.js")}}"></script>
 
     <!-- Page level plugins -->
+    <script src="{{asset("vendor/datatables/jquery.dataTables.min.js")}}"></script>
+    <script src="{{asset("vendor/datatables/dataTables.bootstrap4.min.js")}}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{asset("/js/demo/datatables-demo.js")}}"></script>
+
+    <!-- Page level plugins -->
     <script src="{{asset("vendor/chart.js/Chart.min.js")}}"></script>
 
     <!-- Page level custom scripts -->
